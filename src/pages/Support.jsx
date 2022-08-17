c1d72ebef0345a1ec1bf1146e4ac5d2ec8ab798e
ShakulovElzar <edu31712@gmail.com>
2022-08-17 10:16:07 +0600

    lastы

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -2,12 +2,12 @@ import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 
 const Support = () => {
-    const [reportIssue, setReportIssue] = useState()
+    const [reportIssue, setReportIssue] = useState(1)
     const [reportText, setReportText] = useState("")
-    const [reportDestination, setReportDestination] = useState()
+    const [reportDestination, setReportDestination] = useState(1)
 
-    const [options, setOptions] = useState()
-    const [workers, setWorkers] = useState()
+    const [options, setOptions] = useState([])
+    const [workers, setWorkers] = useState([])
 
     const sendReport = event => {
         event.preventDefault();
@@ -17,9 +17,9 @@ const Support = () => {
         const response = axios.post(
             'http://10.200.24.103:8089/help/create/',
             {
-                "choise": reportIssue,
+                "choice": reportIssue,
                 "description": reportText,
-                "for_whom": 0,
+                "for_whom": reportDestination,
             },
             {
                 headers: {
@@ -29,18 +29,24 @@ const Support = () => {
         );
     }
 
-    // useEffect(() => {
-    //     axios.get("http://10.200.24.103:8089/help/option/", {
-    //         headers: {
-    //             "Authorization": `Bearer ${localStorage.getItem("atoken")}`
-    //         }
-    //     }).then(t => setOptions(t)).then(() => options)
-    //     axios.get("http://10.200.24.103:8089/help/worker/", {
-    //         headers: {
-    //             "Authorization": `Bearer ${localStorage.getItem("atoken")}`
-    //         }
-    //     }).then(t => setWorkers(t)).then(() => workers)
-    // }, [page])
+    useEffect(() => {
+        axios.get("http://10.200.24.103:8089/help/option/", {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("atoken")}`
+            }
+        }).then(t => {
+            setOptions(t.data)
+            console.log(t)
+        })
+        axios.get("http://10.200.24.103:8089/help/worker/", {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("atoken")}`
+            }
+        }).then(t => {
+            setWorkers(t.data)
+            console.log(t)
+        })
+    }, [])
     return (
         <div className='page-body body-box'>
             <div className='help__header'>
@@ -54,19 +60,19 @@ const Support = () => {
                         <select className='inputbox' name="select" onChange={(el) => setReportIssue(el.target.value)}>
                             <option disabled>Выберите пункт</option>
                             {
-                                // options.map((item) => <option key={item.id} value={item.id}>{item.choise}</option>)
+                                options.map((item) => <option key={item.id} value={item.id}>{item.choice}</option>)
                             }
                         </select>
+                        <p className='select-title'>Кому:</p>
                         <select className='inputbox' name="selectt" onChange={(el) => setReportDestination(el.target.value)}>
                             <option disabled>Выберите пункт</option>
                             {
-                                // workers.map((item) => <option key={item.id} value={item.id}>{item.choise}</option>)
+                                workers.map((item) => <option key={item.id} value={item.id}>{item.workername}</option>)
                             }
                         </select>
                         <div className='item-problem'>
                             <p>Опишите свою проблему</p>
                             <textarea className='problem-textarea' placeholder='Сообщение' name="problem" id="problem" value={reportText} onChange={(el) => setReportText(el.target.value)}/>
-                            <p>Кому:</p>
                         </div>
                     </div>
                     <button type='submit'className='form-submit' style={{textTransform: 'uppercase'}}>Отправить</button>
