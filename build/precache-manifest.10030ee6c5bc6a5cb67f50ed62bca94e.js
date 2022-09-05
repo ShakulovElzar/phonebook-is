c434b7f4ee5e990a2052097c9346ca8c8d6a5aac
ShakulovElzar <75471039+ShakulovElzar@users.noreply.github.com>
2022-09-05 14:27:48 +0600

    build added

---
+++ build/precache-manifest.10030ee6c5bc6a5cb67f50ed62bca94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c102bfe96ba24356ecb4",
+    "url": "/static/css/main.a03e61ed.chunk.css"
+  },
+  {
+    "revision": "c102bfe96ba24356ecb4",
+    "url": "/static/js/main.c102bfe9.chunk.js"
+  },
+  {
+    "revision": "b95d2563f1d779decafe",
+    "url": "/static/css/1.e2277d8d.chunk.css"
+  },
+  {
+    "revision": "b95d2563f1d779decafe",
+    "url": "/static/js/1.b95d2563.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee6576719b1ed275f6ca0c560b97ee9d",
+    "url": "/index.html"
+  }
+];
